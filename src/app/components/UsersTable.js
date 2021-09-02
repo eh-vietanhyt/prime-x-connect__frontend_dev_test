d151a69a9a383ede6fc8f8a8df65c165815b34f1
V.A <vietanh@offspringdigital.com>
2021-09-02 09:42:45 +0700

    Add loading for users table when calling api

--- src/app/components/UsersTable.js
+++ src/app/components/UsersTable.js
@@ -8,71 +8,78 @@ import { ButtonGroupWrapper, Pagination } from '../../share/components'
 import { userRowActions, getUserOrganisationFeaturesTxt } from './helpers'
 
 const UsersTable = ({
+  isUpdatingOrganisationUsersList,
   organisationUsersList: {
     data: users
   },
 
   dispatch
-}) => (
-  <>
-    <Table striped bordered hover className="table">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Roles</th>
-          <th>Organisation</th>
-          <th>Organisation Features</th>
-          <th>Country</th>
-          <th className="text-end">Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((user, indexUser) => (
-          <tr key={indexUser}>
-            <td>
-              {user.name}
-            </td>
-            <td>
-              {user.email}
-            </td>
-            <td>
-              {user.roles}
-            </td>
-            <td>
-              {user.organisation}
-            </td>
-            <td>
-              {getUserOrganisationFeaturesTxt(user)}
-            </td>
-            <td>
-              {user.country}
-            </td>
-            <td className="text-end">
-              <ButtonGroupWrapper
-                buttons={userRowActions({ user, dispatch })}
-              />
-            </td>
+}) => {
+  if (isUpdatingOrganisationUsersList) {
+    return <div>Loading spinner here...</div>
+  }
+
+  return (
+    <>
+      <Table striped bordered hover className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Roles</th>
+            <th>Organisation</th>
+            <th>Organisation Features</th>
+            <th>Country</th>
+            <th className="text-end">Action</th>
           </tr>
-        ))}
-        {
-          !users.length
-            ? (
-              <tr>
-                <td colSpan="7">
-                  No data
-                </td>
-              </tr>
-            )
-            : null
-        }
-      </tbody>
-    </Table>
-    <Pagination />
-  </>
-)
+        </thead>
+        <tbody>
+          {
+            !users.length
+              ? (
+                <tr>
+                  <td colSpan="7">
+                    No data
+                  </td>
+                </tr>
+              )
+              : users.map((user, indexUser) => (
+                <tr key={indexUser}>
+                  <td>
+                    {user.name}
+                  </td>
+                  <td>
+                    {user.email}
+                  </td>
+                  <td>
+                    {user.roles}
+                  </td>
+                  <td>
+                    {user.organisation}
+                  </td>
+                  <td>
+                    {getUserOrganisationFeaturesTxt(user)}
+                  </td>
+                  <td>
+                    {user.country}
+                  </td>
+                  <td className="text-end">
+                    <ButtonGroupWrapper
+                      buttons={userRowActions({ user, dispatch })}
+                    />
+                  </td>
+                </tr>
+              ))
+          }
+        </tbody>
+      </Table>
+      <Pagination />
+    </>
+  )
+}
 
 UsersTable.propTypes = {
+  isUpdatingOrganisationUsersList: propTypes.bool,
   organisationUsersList: propTypes.object,
   dispatch: propTypes.func
 }
